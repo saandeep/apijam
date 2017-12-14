489f1ef625e8f0bfba0afddbb892df462d76ed92
Sandeep Murusupalli <sandeepmuru@sandeepmuru-macbookair.roam.corp.google.com>
2017-12-14 10:58:46 +0800

    Updating the script

--- Labs/Pivotal/Lab 7 - Protect the PCF microservices using Apigee Edge Microgateway/resources/restart-microgateways.sh
+++ Labs/Pivotal/Lab 7 - Protect the PCF microservices using Apigee Edge Microgateway/resources/restart-microgateways.sh
@@ -1,3 +1,7 @@
+#You should be logged in as amdin to run this script
+#This script resarts two edge microgateway apps, listening to the same endpoint
+#This will help when you are in a workshop and the attendees are constantly pushing Microgateway api proxies.
+#Restarting the microgateways will automatically pull the changes
 mg1=system-edgemicro-app
 mg2=system-edgemicro-app-2
 cf target -o system -s system
